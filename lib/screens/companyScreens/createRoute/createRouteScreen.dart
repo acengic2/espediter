71d654b69e202b6f1da186070541cbc31943c972
jusuf-el <el.jusuf97@gmail.com>
2020-01-12 15:18:46 +0100

    Logo image se mijenja u zavisnosti od logovanog usera, u nekim dokumentima dodan FINAL u deklaraciji varijabli i neki unsused imports izbaceni

--- lib/screens/companyScreens/createRoute/createRouteScreen.dart
+++ lib/screens/companyScreens/createRoute/createRouteScreen.dart
@@ -41,7 +41,7 @@ const textColorGray60 = Color.fromRGBO(0, 0, 0, 0.6);
 NoRoutes noRoutes = new NoRoutes();
 
 class CreateRoute extends StatelessWidget {
-  String userID;
+  final String userID;
   CreateRoute({this.userID});
   // This widget is the root of your application.
   @override
@@ -67,7 +67,7 @@ class CreateRoute extends StatelessWidget {
 }
 
 class CreateRouteScreenPage extends StatefulWidget {
-  String userID;
+  final String userID;
   CreateRouteScreenPage({Key key, this.userID}) : super(key: key);
 
   @override
